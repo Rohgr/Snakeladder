28cfc912f1aaee500fe61e3d71839ecf866a237b
rohgr <rdagrawal6@gmail.com>
2022-02-23 18:00:49 +0530

    ADDING UC4 SNAKELADDER

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -7,26 +7,26 @@ public class SnakeLadder {
         int currentPosition = 0;
         int IS_SNAKE=2;
         int IS_LADDER=1;
+        int WINNIG_POSITION= 100;
+                while ( currentPosition < WINNIG_POSITION ) {
+                    int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+                    System.out.println(diceNumber);
 
-
-                int diceNumber= (int) (Math.floor(Math.random() * 10) % 6+1);
-            System.out.println(diceNumber);
-
-            int option = (int) (Math.floor(Math.random() * 10) % 3);
-            System.out.println(option);
-            if( option == IS_SNAKE)
-            {
-                currentPosition = currentPosition - diceNumber;
-                System.out.println("snake" +currentPosition);
-            }
-            else if( option == IS_LADDER )
-            {
-                currentPosition = currentPosition + diceNumber;
-                System.out.println("ladder" +currentPosition);
-            }
-            else
-            {
-                System.out.println("No play option");
-            }
+                    int option = (int) (Math.floor(Math.random() * 10) % 3);
+                    System.out.println(option);
+                    if (option == IS_SNAKE) {
+                        currentPosition = currentPosition - diceNumber;
+                        if(currentPosition <0)
+                        {
+                            currentPosition = 0;
+                        }
+                        System.out.println("snake" + currentPosition);
+                    } else if (option == IS_LADDER) {
+                        currentPosition = currentPosition + diceNumber;
+                        System.out.println("ladder" + currentPosition);
+                    } else {
+                        System.out.println("No play option");
+                    }
+                }
     }
 }
