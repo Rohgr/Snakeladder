9a05ef029c5c903fa508d74e45ce88504eda8744
rohgr <rdagrawal6@gmail.com>
2022-02-23 17:49:14 +0530

    Adding UC3 SnakeLadder

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -5,11 +5,28 @@ public class SnakeLadder {
         System.out.println("Welcome");
         int startPosition = 0;
         int currentPosition = 0;
+        int IS_SNAKE=2;
+        int IS_LADDER=1;
 
-            currentPosition = (int) (Math.floor(Math.random() * 10) % 6+1);
-            System.out.println(currentPosition);
 
+                int diceNumber= (int) (Math.floor(Math.random() * 10) % 6+1);
+            System.out.println(diceNumber);
+
+            int option = (int) (Math.floor(Math.random() * 10) % 3);
+            System.out.println(option);
+            if( option == IS_SNAKE)
+            {
+                currentPosition = currentPosition - diceNumber;
+                System.out.println("snake" +currentPosition);
+            }
+            else if( option == IS_LADDER )
+            {
+                currentPosition = currentPosition + diceNumber;
+                System.out.println("ladder" +currentPosition);
+            }
+            else
+            {
+                System.out.println("No play option");
+            }
     }
-
-
 }
