2a189b6db68ec1d09e89009c650194a1bc4ef38f
rohgr <rdagrawal6@gmail.com>
2022-02-23 17:00:01 +0530

    UC2 adding Snakeand Ladder

---
+++ src/SnakeLadder.java
@@ -0,0 +1,15 @@
+
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        int startPosition = 0;
+        int currentPosition = 0;
+
+            currentPosition = (int) (Math.floor(Math.random() * 10) % 6+1);
+            System.out.println(currentPosition);
+
+    }
+
+
+}
