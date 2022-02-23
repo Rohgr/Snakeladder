085da92d91cc7da50965a3449677b89102817bb5
rohgr <rdagrawal6@gmail.com>
2022-02-23 19:22:00 +0530

    Adding UC6 snake and ladder

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,38 +1,55 @@
-
-
 public class SnakeLadder {
-    public static void main(String[] args) {
-        System.out.println("Welcome");
-        int startPosition = 0;
-        int currentPosition = 0;
-        int IS_SNAKE=2;
-        int IS_LADDER=1;
-        int WINNIG_POSITION= 100;
-        int diceCount = 0;
-                while ( currentPosition < WINNIG_POSITION ) {
-                    int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-                    //System.out.println(diceNumber);
+private static final int startPosition = 0;
+private static final  int IS_SNAKE=2;
+private static final int IS_LADDER=1;
+private static final int WINNIG_POSITION= 100;
+private static  int diceCount = 0;
+private static  String turn ="player1";
 
-                    int option = (int) (Math.floor(Math.random() * 10) % 3);
-                    //System.out.println(option);
-                    if (option == IS_SNAKE) {
-                        currentPosition = currentPosition - diceNumber;
-                        if(currentPosition <0)
-                        {
-                            currentPosition = 0;
-                        }
-                        System.out.println("snake" + currentPosition);
-                    } else if (option == IS_LADDER) {
-                        currentPosition = currentPosition + diceNumber;
-                        if(currentPosition > WINNIG_POSITION ){
-                            currentPosition= currentPosition - diceNumber;
-                        }
-                        System.out.println("ladder" + currentPosition);
-                    } else {
-                        System.out.println("No play option");
-                    }
-                    diceCount++;
+        public static void main(String[] args) {
+//        System.out.println("Welcome");
+
+            int currentPositionOfPlayer1 = startPosition;
+            int currentPositionOfPlayer2 = startPosition;
+            while (currentPositionOfPlayer1 < WINNIG_POSITION &&
+                    currentPositionOfPlayer2 < WINNIG_POSITION) {
+                diceCount++;
+                if (turn.equals("player1")) {
+                    turn = "player2";
+                    currentPositionOfPlayer1 = getPosition(currentPositionOfPlayer1);
+                } else {
+                    turn = "player1";
+                    currentPositionOfPlayer2 = getPosition(currentPositionOfPlayer2);
                 }
-                System.out.println("No of Times dice " +diceCount);
-    }
+            }
+            System.out.println("currentPositionofplayer1 " + currentPositionOfPlayer1);
+            System.out.println("currentPositionofplayer2 " + currentPositionOfPlayer2);
+            System.out.println("no of times dice rolled  " +diceCount);
+            if (currentPositionOfPlayer1 == WINNIG_POSITION) {
+                System.out.println("Player 1 win the game");
+            } else {
+                System.out.println("Player 2 win the game");
+            }
+        }
+        private static int getPosition(int currentPosition) {
+            int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+            int option = (int) (Math.floor(Math.random() * 10) % 3);
+            if (option == IS_SNAKE) {
+                currentPosition = currentPosition - diceNumber;
+                if (currentPosition < 0) {
+                    currentPosition = 0;
+                }
+                //System.out.println("snake" + currentPosition);
+            } else if (option == IS_LADDER) {
+               if (currentPosition + diceNumber <= 100) {
+                   currentPosition += diceNumber;
+                }
+                if (turn.equals("player2")) {
+                    turn = "player2";
+                } else if (turn.equals("player1")) {
+                    turn = "player1";
+                }
+            }
+            return currentPosition;
+        }
 }
